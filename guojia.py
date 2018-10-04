995ae7ed6a4397ee8ce0949c3cf72da66ef10d19
lipenghao <272437959@qq.com>
2018-10-04 10:04:47 +0800

    再次提交

--- guojia.py
+++ guojia.py
@@ -1,5 +1,5 @@
 import requests,re
 url='http://www.mm131.com/qipao/'
 html=requests.get(url).text
-a=re.findall('http://www.mm131.com/\w{5,7}/\d{4,6}.html',html)
-print(a)
+a=re.findall('http://www.mm131.com/qipao/\d{4,6}.html',html)
+b=open('D:\py\c5.txt','w')
